cbdeb9987e16ec37038153bb0d7addccb108a0f5
Polonius <benjamin.fellows@gmail.com>
2017-11-26 11:57:58 -0400

    Completed final tutorial section 8: HTTP.

---
+++ angular-tour-of-heroes/src/app/in-memory-data.service.ts
@@ -0,0 +1,21 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDataService implements InMemoryDbService {
+
+  createDb() {
+    const heroes = [
+      { id: 11, name: 'Scubaman' },
+      { id: 12, name: 'Aquatic Calico' },
+      { id: 13, name: 'Jaundice' },
+      { id: 14, name: 'The Argentinian' },
+      { id: 15, name: 'Pillow Fighter' },
+      { id: 16, name: 'Tom Cruise' },
+      { id: 17, name: 'Bridge Troll' },
+      { id: 18, name: 'The Human Plunger' },
+      { id: 19, name: 'Basket Case' },
+      { id: 20, name: 'Destiny\'s Child' }
+    ];
+    return {heroes};
+  }
+
+}
